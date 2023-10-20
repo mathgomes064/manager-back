dbc6253c2f0897753b53392207d336cf2b8f6127
unknown <mathgomes007@hotmail.com>
2023-10-20 10:06:53 -0300

    2ª alteracao build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ echo "Building the Project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "Make Migrations..."
-python3.9 -m manage.py makemigrations --noinput
-python3.9 -m manage.py migrate --noinput
+python3.9 -m manage makemigrations --noinput
+python3.9 -m manage migrate --noinput
 
 echo "Collect Static..."
-python3.9 -m manage.py collectstatic --noinput --clear
+python3.9 -m manage collectstatic --noinput --clear
