35f70e60d9310154fa770b82c00f29be7ebc7759
unknown <mathgomes007@hotmail.com>
2023-10-20 10:32:02 -0300

    4ª tentativa de deploy

--- build.sh
+++ build.sh
@@ -5,13 +5,11 @@ python3.9 -m pip install -U pip
 python3.9 -m pip install -r requirements.txt
 
 # Aplicar migrações de banco de dados
-python3.9 -m manage.py makemigrations
-python3.9 -m manage.py migrate
+python3.9 -m manage makemigrations
+python3.9 -m manage migrate
 
 # Coletar arquivos estáticos
-python3.9 -m manage.py collectstatic --noinput --clear
+python3.9 -m manage collectstatic --noinput --clear
 
-# Outras tarefas específicas do projeto, se necessário
-
-# Iniciar o servidor de desenvolvimento
-python3.9 -m manage.py runserver
+# Iniciar o servidor Gunicorn
+gunicorn _core.wsgi:application --bind 0.0.0.0:8000
