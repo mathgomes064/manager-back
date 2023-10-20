64f42f2af99566c7ec5e1492ab6f896ca1d14616
unknown <mathgomes007@hotmail.com>
2023-10-20 11:37:54 -0300

    7ª tentativa de deploy

--- _core/wsgi.py
+++ _core/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'versel_deploymet.settings')
 
 application = get_wsgi_application()
+
+app = get_wsgi_application()
--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-# Build the project
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
-
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+# build_files.sh
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
